460c4b6e4d82538444839ec774218d4845d78314
hatim ourrach <ourrachhatim0@gmail.com>
2024-01-19 14:16:38 +0100

    manual /movies route

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "main" => "main#index"
+  get "movies" => "movies#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
   resources :movies
   resources :lists
   resources :bookmarks
-
   root to: "main#index"
 
 end
