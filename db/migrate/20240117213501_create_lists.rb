2df82f3482571148e18a13c3ad39453cd94c0e69
hatim ourrach <ourrachhatim0@gmail.com>
2024-01-20 14:35:49 +0100

    fixed list migration

--- db/migrate/20240117213501_create_lists.rb
+++ db/migrate/20240117213501_create_lists.rb
@@ -2,7 +2,6 @@ class CreateLists < ActiveRecord::Migration[7.1]
   def change
     create_table :lists do |t|
       t.string :name
-      t.references :movie, null: false, foreign_key: true
       t.timestamps
     end
   end
